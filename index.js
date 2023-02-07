249226d098ce257b17d162823597b290462837e6
Luiz Dornelas <luizeduardojf@hotmail.com>
2023-02-07 15:23:33 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const movies = require("./api/movies");
 const home = require("./api/home");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.Port || 5000;
 
